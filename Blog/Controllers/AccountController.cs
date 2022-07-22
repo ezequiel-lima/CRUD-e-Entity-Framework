7b1c631000873390c18779ba0257f2d1c24866f3
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-07-22 03:27:32 -0300

    corrigido detalhe

--- Blog/Controllers/AccountController.cs
+++ Blog/Controllers/AccountController.cs
@@ -66,7 +66,6 @@ namespace Blog.Controllers
                 {
                     user = user.Email, 
                     password,
-                    role
                 }));
             }
             catch (DbUpdateException)
